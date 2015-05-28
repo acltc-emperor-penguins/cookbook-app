56980d58afe3c04af5fddcf6dfd4b4cbaf53cbc1
Jay Wengrow <jaywngrw@gmail.com>
2015-05-27 21:31:42 -0500

    display all recipes on a separate page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,8 @@ class RecipesController < ApplicationController
   def home
     @recipe = Recipe.last
   end
+
+  def all
+    @recipes = Recipe.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/" => 'recipes#home'
+  get "/all" => 'recipes#all'
 end
