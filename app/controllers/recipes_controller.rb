3da8d9de6358744b511c0672cec17cc6b7bad9a3
Jay Wengrow <jaywngrw@gmail.com>
2015-05-31 16:49:51 -0500

    implemented show action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,11 @@
 class RecipesController < ApplicationController
   
-
   def index
     @recipes = Recipe.all
+  end
+
+  def show
+    recipe_id = params[:id]
+    @recipe = Recipe.find_by(id: recipe_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/recipes" => 'recipes#index'
+  get "/recipes/:id" => 'recipes#show'
 end
